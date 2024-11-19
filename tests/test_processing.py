f3c3447450f1b08f06d9822e474ea87e62c0e7b6
valerii volynchikov <avto162@gmail.com>
2024-11-19 18:51:38 +0300

    linter

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,4 +1,5 @@
 import pytest
+
 from src.processing import filter_by_state, sort_by_date
 
 # Пример данных для тестов
@@ -9,26 +10,29 @@ data = [
     {"date": "2024-01-02T12:00:00.000000", "state": "EXECUTED", "amount": 400},
 ]
 
-def test_filter_by_state():
+
+def test_filter_by_state() -> None:
     # Тестирование фильтрации по состоянию
     executed_transactions = filter_by_state(data, "EXECUTED")
     assert len(executed_transactions) == 3
-    assert all(item['state'] == "EXECUTED" for item in executed_transactions)
+    assert all(item["state"] == "EXECUTED" for item in executed_transactions)
 
     cancelled_transactions = filter_by_state(data, "CANCELLED")
     assert len(cancelled_transactions) == 1
-    assert cancelled_transactions[0]['state'] == "CANCELLED"
+    assert cancelled_transactions[0]["state"] == "CANCELLED"
 
-def test_sort_by_date():
+
+def test_sort_by_date() -> None:
     # Тестирование сортировки по дате
     sorted_data = sort_by_date(data)
-    assert sorted_data[0]['date'] == "2024-01-05T12:00:00.000000"  # Последняя по дате
-    assert sorted_data[3]['date'] == "2024-01-01T12:00:00.000000"  # Первая по дате
+    assert sorted_data[0]["date"] == "2024-01-05T12:00:00.000000"  # Последняя по дате
+    assert sorted_data[3]["date"] == "2024-01-01T12:00:00.000000"  # Первая по дате
 
     # Проверка сортировки в обратном порядке
     sorted_data_desc = sort_by_date(data, reverse_order=False)
-    assert sorted_data_desc[0]['date'] == "2024-01-01T12:00:00.000000"  # Первая по дате
-    assert sorted_data_desc[3]['date'] == "2024-01-05T12:00:00.000000"  # Последняя по дате
+    assert sorted_data_desc[0]["date"] == "2024-01-01T12:00:00.000000"  # Первая по дате
+    assert sorted_data_desc[3]["date"] == "2024-01-05T12:00:00.000000"  # Последняя по дате
+
 
 # Запуск тестов
 if __name__ == "__main__":
