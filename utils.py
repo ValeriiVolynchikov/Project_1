d016bd42c20255983a75a470a3287781982ce02a
valerii volynchikov <avto162@gmail.com>
2024-12-10 18:18:00 +0300

    modified

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def load_transactions(file_path):
     if not os.path.isfile(file_path):
         return []  # Если файл не существует, вернуть пустой список
 
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         try:
             data = json.load(file)  # Попытка загрузить данные из файла
             if isinstance(data, list):  # Проверка, является ли загруженные данные списком
@@ -15,3 +15,15 @@ def load_transactions(file_path):
             return []  # Если данные не список, вернуть пустой список
         except json.JSONDecodeError:
             return []  # Если ошибка при декодировании JSON, вернуть пустой список
+
+
+# Тестирование функции
+def test_load_transactions():
+    file_path = "data/operations.json"
+    transactions = load_transactions(file_path)
+    print("Loaded transactions:", transactions)
+
+
+# Запустите тест
+if __name__ == "__main__":
+    test_load_transactions()
